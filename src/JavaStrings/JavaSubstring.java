58b68729293519aac23069e4c829cf051dd17ecd
Chevon Fernando <chevonfernando10@gmail.com>
2022-04-27 02:43:31 +0530

    Completed JavaSubstring.java

--- src/JavaStrings/JavaSubstring.java
+++ src/JavaStrings/JavaSubstring.java
@@ -1,4 +1,13 @@
 package JavaStrings;
 
+import java.util.Scanner;
+
 public class JavaSubstring {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String s1 = input.next();
+        int start = input.nextInt();
+        int end = input.nextInt();
+        System.out.println(s1.substring(start,end));
+    }
 }
